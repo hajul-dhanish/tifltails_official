c8bd5b61f80d674bd5a46391303c8895bcadc227
hajul-dhanish <95181356+hajul-dhanish@users.noreply.github.com>
2025-10-04 21:24:21 +0000

    Deploying to gh-pages from @ hajul-dhanish/tifltails_official@df5940eeb71732eddcec73fff9ac526c5b353074 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"d3d45dcf251823c1769909cd43698d126db38d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3398099853"
+    serviceWorkerVersion: "144740837"
   }
 });
